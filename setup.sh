d1d261f945cf3e8291cb1a650acd539d3ba4115d
Egor Kurito <ekurito@gmail.com>
2021-02-19 13:58:51 +0300

    [Add] Add temporary nginx conf files

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+minikube stop
 minikube delete
 minikube start
 minikube dashboard
-# minikube addons enable metallb
-# kubectl apply -f ./srcs/configmap.yaml
+minikube addons enable metallb
+kubectl apply -f ./srcs/configmap.yaml
+eval $(minikube docker-env);
+
+docker build -t nginx_image ./srcs/nginx
+
+kubectl apply -f ./srcs/nginx/nginx.yaml
