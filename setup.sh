c3d891ec8f84aa7b51ed354530dc471558841807
Egor Kurito <ekurito@gmail.com>
2021-03-16 19:16:34 +0300

    [Fix] Full work nginx

--- setup.sh
+++ setup.sh
@@ -1,19 +1,14 @@
-#!/bin/sh
-
 minikube stop
 minikube delete
-minikube start
+minikube start --driver=docker
 
 eval $(minikube docker-env);
-
 minikube addons enable metallb
-
 minikube addons enable metrics-server
+minikube addons list
 
-docker build -t nginx ./srcs/nginx/
-
+docker build -t nginx_image srcs/nginx
 kubectl apply -f ./srcs/configmap.yaml
-
 kubectl apply -f ./srcs/nginx/nginx.yaml
 
 minikube dashboard
--- srcs/nginx/start.sh
+++ srcs/nginx/start.sh
@@ -1,12 +1,3 @@
-#!/bin/sh
+ssh-keygen -A
 
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 365 -keyout /etc/ssl/nginx_cert.key -out /etc/ssl/nginx_cert.crt < input
-rm input
-rc-status
-rc-service nginx restart
-echo -e "1234\n1234\n" > input
-adduser hanguy < input
-rm input
-rc-service sshd restart
-/usr/bin/telegraf &
-sleep infinity
+/usr/bin/supervisord -c /etc/supervisord.conf
