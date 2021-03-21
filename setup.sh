e3edea460c87494c5bd2280e82b92270b418f13f
Egor Kurito <ekurito@gmail.com>
2021-03-21 19:26:53 +0300

    [Fix] Some Fix

--- setup.sh
+++ setup.sh
@@ -9,25 +9,20 @@ minikube addons enable metrics-server
 minikube addons list
 
 docker build -t nginx_image srcs/nginx
+docker build -t mysql_image srcs/mysql
+docker build -t phpmyadmin_image srcs/phpmyadmin
+docker build -t wordpress_image srcs/wordpress
+docker build -t ftps_image srcs/ftps
+docker build -t influxdb_image srcs/influxdb
+docker build -t grafana_image srcs/grafana
+
 kubectl apply -f ./srcs/configmap.yaml
 kubectl apply -f ./srcs/nginx/nginx.yaml
-
-docker build -t mysql_image srcs/mysql
 kubectl apply -f ./srcs/mysql/mysql.yaml
-
-docker build -t phpmyadmin_image srcs/phpmyadmin
 kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
-
-docker build -t wordpress_image srcs/wordpress
 kubectl apply -f ./srcs/wordpress/wordpress.yaml
-
-docker build -t ftps_image srcs/ftps
 kubectl apply -f ./srcs/ftps/ftps.yaml
-
-docker build -t influxdb_image srcs/influxdb
 kubectl apply -f ./srcs/influxdb/influxdb.yaml
-
-docker build -t grafana_image srcs/grafana
 kubectl apply -f ./srcs/grafana/grafana.yaml
 
 minikube dashboard
--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -3,11 +3,8 @@
 mariadb-install-db --user=root
 
 mysqld --user=root & sleep 5
-
 mysql --user=root --execute="CREATE DATABASE wordpress;"
-
 mysql --user=root wordpress < wordpress.sql
-
 mysql --user=root --execute="CREATE USER 'root'@'%' IDENTIFIED BY 'pass'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; USE wordpress; FLUSH PRIVILEGES;"
 
 /usr/bin/supervisord -c /etc/supervisord.conf
